f8efb892277796cd78adcb5a8e7afc51db7af2a4
rlandreth14-netizen <r.landreth14@gmail.com>
2026-06-24 11:09:26 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
    bump the VERSION string below.
    ════════════════════════════════════════════════════════════════ */
 
-const VERSION = 'cutnest-v12';
+const VERSION = 'cutnest-v14';
 
 const PRECACHE = [
   '/',
